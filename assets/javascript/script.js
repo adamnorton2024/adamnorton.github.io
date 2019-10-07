6bbf814ac5f531eab0e8781e2273fb9042ca7b85
Adam Norton <adam@adamnorton.com>
2019-10-06 20:43:01 -0500

    initial file add and basic layout of new portfolio site.

---
+++ assets/javascript/script.js
@@ -0,0 +1,4 @@
+var backArr = ["url(./assets/images/background01.jpg)", "url(./assets/images/background02.jpg)", "url(./assets/images/background03.jpg)" ];
+var randomBackgroundNumber = Math.floor(Math.random() * backArr.length);
+
+//
